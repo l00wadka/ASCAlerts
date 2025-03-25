109c8051082101b61b0b766a9c4a9e689ecb08fd
lowadka <138142698+l00wadka@users.noreply.github.com>
2025-03-25 19:29:41 +0700

    Update config.py

--- ASCALERTS/config.py
+++ ASCALERTS/config.py
@@ -1,4 +1,4 @@
 BOT_TOKEN = "PASTE_HERE" # CREATE HERE @BotFather
 API_KEY = "PASTE_HERE" # Create it on https://api.rarible.org/dashboard/
-OWNER_ID = 361872658  # Replace with your Telegram user ID @userinfobot
+OWNER_ID = 111111111  # Replace with your Telegram user ID @userinfobot
 SLEEP_TIME = 30  # Activity refresh rate
